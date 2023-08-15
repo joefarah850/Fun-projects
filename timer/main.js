c3d4e3989bc1c7522e54f32d26551267ef3532d1
joefarah850 <85506290+joefarah850@users.noreply.github.com>
2023-08-15 13:30:52 +0300

    Update main.js

--- timer/main.js
+++ timer/main.js
@@ -1,5 +1,5 @@
-var countDownDate = new Date("May 12, 2023 21:00:00").getTime()
-var countDownDate2 = new Date("June 12, 2023 21:00:00").getTime()
+var countDownDate = new Date("December 23, 2023 21:00:00").getTime()
+var countDownDate2 = new Date("December 23, 2023 21:00:00").getTime()
 
 var x = setInterval(function() {
 
